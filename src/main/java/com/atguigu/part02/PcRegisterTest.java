958cdcb25e9904563bf8b9f4fbd8d27d21337760
lucky845 <lc1447545564@gmail.com>
2022-03-25 20:30:28 +0800

    测试PC寄存器

---
+++ src/main/java/com/atguigu/part02/PcRegisterTest.java
@@ -0,0 +1,19 @@
+package com.atguigu.part02;
+
+/**
+ * 测试PC寄存器
+ *
+ * @author lucky845
+ * @date 2022年03月25日 16:24
+ */
+public class PcRegisterTest {
+
+    public static void main(String[] args) {
+        int i = 10;
+        // long kkk = 12332123;
+        int j = 20;
+        int m = i + j;
+        new Thread().start();
+    }
+
+}
