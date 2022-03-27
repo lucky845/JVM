e142f30a7ca4a085aed38cc32e84f6b4005fab77
lucky845 <lc1447545564@gmail.com>
2022-03-27 14:11:10 +0800

    测试运行时常量池

---
+++ src/main/java/com/atguigu/part06/Test02_ConstPool.java
@@ -0,0 +1,13 @@
+package com.atguigu.part06;
+
+/**
+ * @author lucky845
+ * @date 2022年03月27日 14:09
+ */
+public class Test02_ConstPool {
+    // 查看字节码文件中的运行时常量池
+    public static void main(String[] args) {
+        System.out.println("lucky845");
+    }
+
+}
